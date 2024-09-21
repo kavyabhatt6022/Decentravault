e8051bc87e90716babd8fc06ef6ee4ca6fe22f88
kavyabhatt6022 <121529828+kavyabhatt6022@users.noreply.github.com>
2024-09-21 19:14:04 +0530

    Initial commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,20 @@
+//Importing Hardhat
 const hre = require("hardhat");
 
+//Defining the main function is an asynchronous function that will be executed when the script is run.
 async function main() {
   
+  // the deployContract method from hre.ethers is used to deploy a smart contract named "Upload". The await keyword is used to wait for the deployment to complete.
   const upload = await hre.ethers.deployContract("Upload");
+  //Waiting for deployment
   await upload.waitForDeployment();
 
+  //Logging the deployment address: The address is retrieved from the target property of the upload object.
   console.log("Library deployed to:", upload.target);
 }
 
 main().catch((error) => {
+  //console.error, the process exit code is set to 1, indicating that an error occurred.
   console.error(error);
   process.exitCode = 1;
 });
